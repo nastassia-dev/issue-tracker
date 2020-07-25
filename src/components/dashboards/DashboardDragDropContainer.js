c5258579427caea4c7c639eeb6568f16fa133d48
nastassia-dev <nastassia.dev@gmail.com>
2020-07-24 21:57:19 -0400

    Add task drag and drop on frontend

--- src/components/dashboards/DashboardDragDropContainer.js
+++ src/components/dashboards/DashboardDragDropContainer.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import Grid from '@material-ui/core/Grid';
 
 import DroppableColumn from './DroppableColumn';
 
-const DashboardDragDropContainer = ({ dashboard: { columns, tasks, columnOrder } }) => {
+const DashboardDragDropContainer = ({ dashboard }) => {
+	const { columns, tasks, columnOrder } = dashboard;
+	const [all, setAll] = useState(dashboard);
+
 	const onDragStart = () => {};
 	const onDragUpdate = () => {};
-	const onDragEnd = () => {};
+	const onDragEnd = result => {
+		const { destination, source, draggableId } = result;
+		if (!destination) return;
+		if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+		const startColumn = all.columns.find(c => c.id === source.droppableId);
+		const finishColumn = all.columns.find(c => c.id === destination.droppableId);
+
+		if (startColumn === finishColumn) {
+			const taskIds = [...startColumn.taskIds];
+			taskIds.splice(source.index, 1);
+			taskIds.splice(destination.index, 0, draggableId);
+			const column = { ...startColumn, taskIds };
+			setAll({
+				...all,
+				columns: all.columns.map(c => (c.id === column.id) ? column : c),
+			});
+			return;
+		}
+		const sourceTaskIds = [...startColumn.taskIds];
+		const destinationTaskIds = [...finishColumn.taskIds];
+		sourceTaskIds.splice(source.index, 1);
+		destinationTaskIds.splice(destination.index, 0, draggableId);
+		const newColumns = {
+			[startColumn.id]: { ...startColumn, taskIds: sourceTaskIds },
+			[finishColumn.id]: { ...finishColumn, taskIds: destinationTaskIds }
+		};
+		setAll({
+			...all,
+			columns: all.columns.map(c => newColumns[c.id] ? newColumns[c.id] : c),
+		});
+	};
 
 	return (
 		<DragDropContext
@@ -17,8 +51,8 @@ const DashboardDragDropContainer = ({ dashboard: { columns, tasks, columnOrder }
 		>
 			<Grid container justify='flex-start' spacing={1}>
 				{columnOrder.map((columnId, index) => {
-					const column = columns.find(c => c.id === columnId);
-					const ownTasks = tasks.filter(t => t.columnId === columnId);
+					const column = all.columns.find(c => c.id === columnId);
+					const ownTasks = column.taskIds.map(t => all.tasks.find(tt => tt.id === t));
 
 					return (
 						<Grid item key={columnId}>
